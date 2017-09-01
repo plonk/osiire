351313310d721d871e853ead5582ef7e5c5307e3
Yoteichi <plonk@piano.email.ne.jp>
2017-09-02 08:09:16 +0900

    リファクタリング。

--- dungeon.rb
+++ dungeon.rb
@@ -1,34 +1,49 @@
 # aka LevelGenerator
 class Dungeon
-  def make_level(level_number, hero)
-    fail unless level_number.is_a? Integer and level_number >= 1
-    level = Level.new
-
-    # 階段を置く。
-    level.put_object(*level.get_random_place(:FLOOR), StairCase.new)
-
-    # 金を置く。
+  # 金を置く。
+  def place_gold(level)
     5.times do
       cell = level.cell(*level.get_random_place(:FLOOR))
       if cell.objects.empty?
         cell.objects << Gold.new(rand(100..1000))
       end
     end
+  end
 
+  # 階段を置く。
+  def place_stair_case(level)
+    level.put_object(*level.get_random_place(:FLOOR), StairCase.new)
+  end
+
+  # ナンを置く
+  def place_food(level)
     5.times do
       cell = level.cell(*level.get_random_place(:FLOOR))
       if cell.objects.empty?
         cell.objects << Item.make_item("ナン")
       end
     end
+  end
 
-    # モンスターを配置する。
+  # モンスターを配置する。
+  def place_monsters(level)
     5.times do
       cell = level.cell(*level.get_random_place(:FLOOR))
       if cell.objects.none? { |obj| obj.is_a? Monster }
         cell.objects << Monster.make_monster('まんまる')
       end
     end
+  end
+
+  def make_level(level_number, hero)
+    fail unless level_number.is_a? Integer and level_number >= 1
+
+    level = Level.new
+
+    place_stair_case(level)
+    place_gold(level)
+    place_food(level)
+    place_monsters(level)
 
     return level
   end
