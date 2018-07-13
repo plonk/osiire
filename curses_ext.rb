673221c067a516ccd71fda3ed3a14bf6bc4cdc14
Yoteichi <plonk@piano.email.ne.jp>
2018-07-14 05:15:34 +0900

    ウィンドウの枠をグラフィックにした。

--- curses_ext.rb
+++ curses_ext.rb
@@ -7,16 +7,33 @@ module Curses
 end
 
 class Curses::Window
+  # def rounded_box
+  #   win = self
+  #   win.box("\0", "\0")
+  #   win.setpos(0,0)
+  #   win.addstr("╭")
+  #   win.setpos(0, win.maxx-1)
+  #   win.addstr("╮")
+  #   win.setpos(win.maxy-1, 0)
+  #   win.addstr("╰")
+  #   win.setpos(win.maxy-1,win.maxx-1)
+  #   win.addstr("╯")
+  # end
+
   def rounded_box
-    win = self
-    win.box("\0", "\0")
-    win.setpos(0,0)
-    win.addstr("╭")
-    win.setpos(0, win.maxx-1)
-    win.addstr("╮")
-    win.setpos(win.maxy-1, 0)
-    win.addstr("╰")
-    win.setpos(win.maxy-1,win.maxx-1)
-    win.addstr("╯")
+    box("\0", "\u{104231}")
+    setpos(0,0)
+    addstr("\u{104230}")
+    addstr("\u{104231}" * (maxx - 2))
+    addstr("\u{104232}")
+    (1..(maxy-2)).each do |y|
+      setpos(y, 0)
+      addstr("\u{104233}")
+      setpos(y, maxx-1)
+      addstr("\u{104235}")
+    end
+    addstr("\u{104234}")
+    addstr("\u{104237}" * (maxx - 2))
+    addstr("\u{104236}")
   end
 end
